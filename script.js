1931a4161cdebaead73c649d431edf598805e3d2
Staney Shen <stanley_shen@hotmail.com>
2023-03-15 09:02:18 -0300

    https://github.com/stanleyshensantos/stanleyshensantos

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 async function buscaEndereco(){
     var consultaCep = await fetch('https://viacep.com.br/ws/01001000/json/')
-
+    var consultaCEPConvertida = await consultaCep.json();
+    console.log(consultaCEPConvertida);
 }
 
 
-console.log(consultaCep);
+buscaEndereco();
