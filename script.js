52fceff8db22644d0718bf40a66e48a0e69b2027
Staney Shen <stanley_shen@hotmail.com>
2023-03-16 14:49:21 -0300

    https://github.com/stanleyshensantos/jconsumindo-dados

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
-async function buscaEndereco(){
-    var consultaCep = await fetch('https://viacep.com.br/ws/01001000/json/')
+async function buscaEndereco(cep){
+    var mensagemErro = document.getElementById('erro');
+    mensagemErro.innerHTML = "";
+    try {
+    var consultaCep = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
     var consultaCEPConvertida = await consultaCep.json();
+    if(consultaCEPConvertida.erro){
+        throw Error('CEP não existente!');
+    }
+    var cidade = document.getElementById('cidade');
+    var logradouro = document.getElementById('endereco');
+    var estado = document.getElementById('estado');
+
+    cidade.value = consultaCEPConvertida.localidade;
+    logradouro.value = consultaCEPConvertida.logradouro;
+    estado.value = consultaCEPConvertida.uf;
+
     console.log(consultaCEPConvertida);
+    return consultaCEPConvertida;
+    
+}catch (erro){
+        mensagemErro.innerHTML = `<p>CEP inválido. Tente novamente!</p>`
+        console.log(erro)
+    }
 }
-
-
-buscaEndereco();
+var cep = document.getElementById('cep');
+cep.addEventListener("focusout",() => buscaEndereco(cep.value));
